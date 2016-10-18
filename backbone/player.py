5645a147a3bf954783f18d2f87253ec1820a6b74
xp0ltergiestx <callum.lairdx23@gmail.com>
2016-10-18 20:34:15 +0100

    Update player.py

--- backbone/player.py
+++ backbone/player.py
@@ -1,7 +1,7 @@
 from items import *
 from map import rooms
 
-inventory = [item_laptop, item_money]
+inventory = [item_laptop, item_money,item_smartphone]
 
 # Start game at the reception
 current_room = rooms["Reception"]
